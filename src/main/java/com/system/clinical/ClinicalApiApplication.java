eedfb942ecee131aa121fe5db977afae8ee6d494
William Gomes <william.desenv@gmail.com>
2021-04-03 16:09:35 -0400

    Adicionando projeto ao Git

---
+++ src/main/java/com/system/clinical/ClinicalApiApplication.java
@@ -0,0 +1,14 @@
+package com.system.clinical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ClinicalApiApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/clinical/ClinicalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.clinical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
